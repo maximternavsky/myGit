74c179b4dcd2f193d9673c1d3bde1823845210b4
maxim.ternavsky <maxim.ternavsky@gmail.com>
2020-05-14 21:12:56 +0300

    Создал класс Int для упражнения 1 главы 6

---
+++ Lessons/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+#include <ostream>
+#include <istream>
+
+
+using namespace std;
+
+class Int
+{
+    int x;
+public:
+    Int()  
+    {
+        x = 0;
+    }
+
+    Int(int a) : x(a)
+    {}
+
+    void set(int a)
+    {
+        x = a;
+    }
+
+    Int operator+(Int& a) const
+    {
+        Int b(x + a.x);
+
+        return b;
+    }
+
+    void setInt(int a) 
+    {
+        x=a;
+    }
+
+    int getX()
+    {
+        return x;
+    }
+
+    
+
+};
+
+ostream& operator << (ostream& os, Int a)
+{
+    os << a.getX();
+    return os;
+}
+
+
+int main()
+{
+    Int i(5);
+    cout << i;
+}
