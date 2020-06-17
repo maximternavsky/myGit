d940d7afd9ac2776d085b067d3cbc4660cf23e26
Maxim <maxim.ternavsky@gmail.com>
2020-06-17 21:15:06 +0300

    Solution kata "Count by X" from Codewars.com

--- Lessons/Lesson/Lessons/Lessons/Source.cpp
+++ Lessons/Lesson/Lessons/Lessons/Source.cpp
@@ -1,173 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+std::vector<int> countBy(int x, int n) {
 
+	int xBefore = x;
+	int mult = x * n;
+	std::vector<int> array;
 
-using namespace std;
-
-
-#include <string>
-using namespace std;
-
-string getFruit(int num);
-
-string SubtractSum(int n)
-{
-	int summ;
-	string str;
-	int length;
-
-	while (n > 100)
+	array.push_back(x);
+	while (x < mult)
 	{
-		summ = 0;
-		str = to_string(n);
-		length = str.length() - 1;
-		
-		for (int i = 0, j = length; i < length + 1; i++, j--)
-		{
-			summ += n / (int)((i == length) ? 1 : pow(10, j)) % 10;
-		}
-
-		n = n - summ;
-
+		array.push_back(x + xBefore);
+		x += xBefore;
 	}
 
-	return getFruit(n);
+
+	return array;
 }
-
-string getFruit(int num)
-{
-	string str[] = {
-		"",
-		"kiwi",
-		"pear",
-		"kiwi",
-		"banana",
-		"melon",
-		"banana",
-		"melon",
-		"pineapple",
-		"apple",
-		"pineapple",
-		"cucumber",
-		"pineapple",
-		"cucumber",
-		"orange",
-		"grape",
-		"orange",
-		"grape",
-		"apple",
-		"grape",
-		"cherry",
-		"pear",
-		"cherry",
-		"pear",
-		"kiwi",
-		"banana",
-		"kiwi",
-		"apple",
-		"melon",
-		"banana",
-		"melon",
-		"pineapple",
-		"melon",
-		"pineapple",
-		"cucumber",
-		"orange",
-		"apple",
-		"orange",
-		"grape",
-		"orange",
-		"grape",
-		"cherry",
-		"pear",
-		"cherry",
-		"pear",
-		"apple",
-		"pear",
-		"kiwi",
-		"banana",
-		"kiwi",
-		"banana",
-		"melon",
-		"pineapple",
-		"melon",
-		"apple",
-		"cucumber",
-		"pineapple",
-		"cucumber",
-		"orange",
-		"cucumber",
-		"orange",
-		"grape",
-		"cherry",
-		"apple",
-		"cherry",
-		"pear",
-		"cherry",
-		"pear",
-		"kiwi",
-		"pear",
-		"kiwi",
-		"banana",
-		"apple",
-		"banana",
-		"melon",
-		"pineapple",
-		"melon",
-		"pineapple",
-		"cucumber",
-		"pineapple",
-		"cucumber",
-		"apple",
-		"grape",
-		"orange",
-		"grape",
-		"cherry",
-		"grape",
-		"cherry",
-		"pear",
-		"cherry",
-		"apple",
-		"kiwi",
-		"banana",
-		"kiwi",
-		"banana",
-		"melon",
-		"banana",
-		"melon",
-		"pineapple",
-		"apple",
-		"pineapple"};
-
-		return str[num];
-}
-
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 
-
-	
-	cout << SubtractSum(10) << endl;
-	cout << SubtractSum(1086) << endl;
-	cout << SubtractSum(143450) << endl;
-	cout << SubtractSum(1760) << endl;
-	cout << SubtractSum(8690) << endl;
-	cout << SubtractSum(789) << endl;
-	cout << SubtractSum(7432) << endl;
-	cout << SubtractSum(452) << endl;
-	cout << SubtractSum(567567) << endl;
-	cout << SubtractSum(355) << endl;
-	cout << SubtractSum(234) << endl;
-	cout << SubtractSum(754634) << endl;
-	cout << SubtractSum(1323) << endl;
-	cout << SubtractSum(345) << endl;
-
-
-
-	
+	countBy(100, 5);
 
 	return 0;
 
