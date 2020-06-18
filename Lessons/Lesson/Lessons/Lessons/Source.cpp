4cb319c3d72a75a73b16baa565429e4b8e36b1d4
Maxim <maxim.ternavsky@gmail.com>
2020-06-18 20:09:31 +0300

    Solution next kata

--- Lessons/Lesson/Lessons/Lessons/Source.cpp
+++ Lessons/Lesson/Lessons/Lessons/Source.cpp
@@ -27,11 +27,22 @@ int positive_sum(const std::vector<int> arr) {
 	return summ;
 }
 
+string sliceString(string str)
+{
+	
+		char buf[30];
+		size_t n = str.copy(buf, str.length() - 2, 1);
+		buf[n] = '\0';	
+	
+
+	return buf;
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 
-
+	cout << sliceString("country") << endl;
 
 	return 0;
 
