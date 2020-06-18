418cc7d568f58276178f022f9eeeab0238bafb7d
Maxim <maxim.ternavsky@gmail.com>
2020-06-18 14:02:26 +0300

    Solution kata "Beginner Series #2 Clock" from Codewars.com

--- Lessons/Lesson/Lessons/Lessons/Source.cpp
+++ Lessons/Lesson/Lessons/Lessons/Source.cpp
@@ -3,24 +3,8 @@
 
 using namespace std;
 
-unsigned short int expressionsMatter(unsigned short int a, unsigned short int b, unsigned short int c)
-{
-	vector<unsigned short int> results;
-
-	unsigned short int bigNumber = 0;
-
-	results.push_back(a * b * c);
-	results.push_back(a + b + c);
-	results.push_back((a + b) * c);
-	results.push_back(a * b + c);
-	for (unsigned short int i : results)
-	{
-		if (i > bigNumber)
-		{
-			bigNumber = i;
-		}
-	}
-	return bigNumber;
+int past(int h, int m, int s) {
+	return (h * 3600000 + m * 60000 + s * 1000);
 }
 
 int main()
