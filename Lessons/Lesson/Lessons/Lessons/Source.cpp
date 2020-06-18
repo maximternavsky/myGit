90751a7220a153eb99ff1a72e44b8a1abb587a28
Maxim <maxim.ternavsky@gmail.com>
2020-06-18 14:09:56 +0300

    Add solution kata "Even or Odd"

--- Lessons/Lesson/Lessons/Lessons/Source.cpp
+++ Lessons/Lesson/Lessons/Lessons/Source.cpp
@@ -7,6 +7,11 @@ int past(int h, int m, int s) {
 	return (h * 3600000 + m * 60000 + s * 1000);
 }
 
+std::string even_or_odd(int number)
+{
+	return ((number % 2) != 0) ? "Odd" : "Even";
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
